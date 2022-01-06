589581d7c15063adcd11b0837700252eb8ab6fe8
Xavier J <xmj359@gmail.com>
2022-01-06 11:39:12 -0600

    commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+
+class Main extends Component {
+
+    constructor(props){
+        super(props);
+        this.state= {};
+    }
+
+    render() {
+        return(
+            <p>Hello World</p>
+        );
+    }
+}
+
+export default Main;
